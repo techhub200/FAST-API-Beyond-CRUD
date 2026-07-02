5d935d910bbde2313a7f2ee5068c615b78bdadf1
techhub200 <divyanshupant155@gmail.com>
2026-07-02 14:24:36 +0530

    Remove Version from API

--- src/__init__.py
+++ src/__init__.py
@@ -2,14 +2,13 @@ from fastapi import FastAPI
 from src.books.routes import book_router
 app=FastAPI()
 
-version ="V1"
 
 app = FastAPI(
     title="BOOKlY",
     description="RESAPI for BookDEsign",
-    version=version
+    
 )
 
 app.include_router(  book_router,
-    prefix=f"/api/{version}/books",
+    prefix=f"/api/books",
     tags=["books"])
