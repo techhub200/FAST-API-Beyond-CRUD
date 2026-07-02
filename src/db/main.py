7cc2fa6b360681c2bfa0d9cab81ce829c4bf85f6
techhub200 <divyanshupant155@gmail.com>
2026-07-02 23:10:24 +0530

    d

--- src/db/main.py
+++
@@ -1,9 +0,0 @@
-from sqlmodel import create_engine
-from sqlalchemy.ext.asyncio import AsyncEngine
-from src.config import Config
-
-engine=create_engine(
-      create_engine(url=  Config,
-      echo=True
-      )
-)
