858a3582e109655122045c9a9a5205bbf7e50f2a
techhub200 <divyanshupant155@gmail.com>
2026-07-06 19:55:27 +0530

    Securing the Database configuration

--- src/config.py
+++ src/config.py
@@ -2,8 +2,10 @@
 from sqlalchemy import create_engine #Imports the SQLAlchemy function used to create a connection to the database.
 from sqlalchemy.orm import sessionmaker  #Imports SQLAlchemy’s session factory, which helps create database sessions for queries.
 
-DATABASE_URL = "postgresql+psycopg://postgres:Postgre%402004@localhost:5432/BOOK_DATABASE"
-
+from dotenv import load_dotenv  #Imports the load_dotenv function from the python-dotenv package, which loads environment variables from a .env file.
+import os
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL, echo=True)
 
 
