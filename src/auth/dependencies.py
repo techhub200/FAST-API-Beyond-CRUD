97b2b2a5ead6f999eb4cf2f096ac029a619c0f02
techhub200 <divyanshupant155@gmail.com>
2026-07-08 19:47:48 +0530

    Override the security function to verify jwt

--- src/auth/dependencies.py
+++ src/auth/dependencies.py
@@ -1,4 +1,14 @@
 from fastapi.security import HTTPBearer
+from fastapi import HTTPException, Request
+from fastapi.security.http import HTTPAuthorizationCredentials
+from src.auth.utils import decode_access_token
 
 class Access_Token_Bearer(HTTPBearer):
-    pass
+    async def __call__(self, request: Request):
+        credentials: HTTPAuthorizationCredentials = await super().__call__(request)
+
+        try:
+            payload = decode_access_token(credentials.credentials)
+            return payload
+        except Exception:
+            raise HTTPException(status_code=401, detail="Invalid or expired token")
